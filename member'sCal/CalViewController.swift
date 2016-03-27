70bb241334e6fed07722f0c1b7da4a6def647504
Junichi Sakonaka <junichi.sakonaka@gmail.com>
2016-03-27 21:50:43 +0900

    マージしました。ストーリーボードはコピペしただけです。

--- member'sCal/CalViewController.swift
+++ member'sCal/CalViewController.swift
@@ -12,6 +12,18 @@ class CalViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIC
     @IBOutlet weak var calDisplay: UICollectionView!
     @IBOutlet weak var workDisplay: UITableView!
     
+    // MARK: - Properties
+    @IBOutlet weak var calendarView: CVCalendarView!
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var monthLabel: UILabel!
+    //    @IBOutlet weak var daysOutSwitch: UISwitch!
+    
+    var shouldShowDaysOut = true
+    var animationFinished = true
+    
+    var selectedDay:DayView!
+    
+    // MARK: - Life cycle
     
     
     override func viewDidLoad() {
@@ -26,7 +38,17 @@ class CalViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIC
 //            return CGSize(width:size,height:size)
 //        print("Ok")
 //        }
-}
+        self.menuView.firstWeekday = .Monday //.Sunday
+        //TODO: 画面サイズが小さい時はフォントサイズを小さくする必要があるので画面サイズ判定での調整を追加する必要がある
+        //monthLabel.font = UIFont.systemFontOfSize(CGFloat(15))
+        monthLabel.font = UIFont.boldSystemFontOfSize(CGFloat(15))
+        monthLabel.text = CVDate(date: NSDate()).globalDescription
+        
+        //TODO: 今月以外の日にちをタップしてその月にスクロールしても、タップした日にちが選択されないので非表示にしておく。不具合解消したらON
+        calendarView.changeDaysOutShowingState(true)
+        shouldShowDaysOut = false
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -36,29 +58,27 @@ class CalViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIC
     override func viewWillAppear(animated: Bool) {
 //            var ad = UIApplication.sharedApplication().delegate as! AppDelegate
         
-        
-        
-        
-        
-        
-        
-        
 //        calDisplay.reloadData()
         workDisplay.reloadData()
         
     }
     
     override func viewDidLayoutSubviews() {
-        func collectionView(collectionView:UICollectionView,layout collectionViewLayout:UICollectionViewLayout,sizeForItemAtIndexPath indexPath:NSIndexPath) ->CGSize{
-            let size = calDisplay.frame.size.width / 20
-            return CGSize(width:size,height:size)
-            
-//            NSLog("bbbbbbb")
-//            let size = calDisplay.frame.size.width / 10//7 - 1 return CGSizeMake(size,size)
-        }
+        super.viewDidLayoutSubviews()
+        
+        
+        calendarView.commitCalendarViewUpdate()
+        menuView.commitMenuViewUpdate()
     }
     
     
+    func collectionView(collectionView:UICollectionView,layout collectionViewLayout:UICollectionViewLayout,sizeForItemAtIndexPath indexPath:NSIndexPath) ->CGSize{
+        let size = calDisplay.frame.size.width / 20
+        return CGSize(width:size,height:size)
+        
+//        NSLog("bbbbbbb")
+//        let size = calDisplay.frame.size.width / 10//7 - 1 return CGSizeMake(size,size)
+    }
     
     
 //---------------------------------------------------------------------------------コレクションビュー処理
@@ -80,21 +100,6 @@ class CalViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIC
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-    
-    
-    
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
@@ -129,66 +134,285 @@ class CalViewController: UIViewController,UICollectionViewDelegateFlowLayout,UIC
         
     }
     
+}
+
+
+
+// MARK: - CVCalendarViewDelegate & CVCalendarMenuViewDelegate
+
+extension CalViewController: CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
+    
+    /// Required method to implement!
+    func presentationMode() -> CalendarMode {
+        return .MonthView
+    }
+    
+    /// Required method to implement!
+    func firstWeekday() -> Weekday {
+        return .Monday//.Sunday
+    }
+    
+    // MARK: Optional methods
+    
+    func shouldShowWeekdaysOut() -> Bool {
+        return shouldShowDaysOut
+    }
+    
+    func shouldAnimateResizing() -> Bool {
+        return true // Default value is true
+    }
+    
+    //TODO: 不必要に何度もコールされているので、必用時以外を無処理でスルーする判定が必要
+    func didSelectDayView(dayView: CVCalendarDayView, animationDidFinish: Bool) {
+        print("\(dayView.date.commonDescription) is selected!")
+        selectedDay = dayView
+    }
+    
+//    func pushDayView() {
+//        let dayViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("DayView") as! DayViewController
+//        //pushViewController(dayViewController, animated: true)
+//        presentViewController(dayViewController, animated: true, completion: nil)
+//        
+//    }
+    
+    func presentedDateUpdated(date: CVDate) {
+        if monthLabel.text != date.globalDescription && self.animationFinished {
+            let updatedMonthLabel = UILabel()
+            updatedMonthLabel.textColor = monthLabel.textColor
+            updatedMonthLabel.font = monthLabel.font
+            updatedMonthLabel.textAlignment = .Center
+            updatedMonthLabel.text = date.globalDescription
+            updatedMonthLabel.sizeToFit()
+            updatedMonthLabel.alpha = 0
+            updatedMonthLabel.center = self.monthLabel.center
+            
+            let offset = CGFloat(48)
+            updatedMonthLabel.transform = CGAffineTransformMakeTranslation(0, offset)
+            updatedMonthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+            
+            UIView.animateWithDuration(0.35, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                self.animationFinished = false
+                self.monthLabel.transform = CGAffineTransformMakeTranslation(0, -offset)
+                self.monthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+                self.monthLabel.alpha = 0
+                
+                updatedMonthLabel.alpha = 1
+                updatedMonthLabel.transform = CGAffineTransformIdentity
+                
+                }) { _ in
+                    
+                    self.animationFinished = true
+                    self.monthLabel.frame = updatedMonthLabel.frame
+                    self.monthLabel.text = updatedMonthLabel.text
+                    self.monthLabel.transform = CGAffineTransformIdentity
+                    self.monthLabel.alpha = 1
+                    updatedMonthLabel.removeFromSuperview()
+            }
+            
+            self.view.insertSubview(updatedMonthLabel, aboveSubview: self.monthLabel)
+        }
+    }
+    
+    func topMarker(shouldDisplayOnDayView dayView: CVCalendarDayView) -> Bool {
+        return true
+    }
+    
+    func dotMarker(shouldShowOnDayView dayView: CVCalendarDayView) -> Bool {
+        let day = dayView.date.day
+        //        let randomDay = Int(arc4random_uniform(31))
+        //        if day == randomDay {
+        //            return true
+        //        }
+        
+        return false
+    }
+    
+    func dotMarker(colorOnDayView dayView: CVCalendarDayView) -> [UIColor] {
+        
+        let red = CGFloat(arc4random_uniform(600) / 255)
+        let green = CGFloat(arc4random_uniform(600) / 255)
+        let blue = CGFloat(arc4random_uniform(600) / 255)
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        let numberOfDots = Int(arc4random_uniform(3) + 1)
+        switch(numberOfDots) {
+        case 2:
+            return [color, color]
+        case 3:
+            return [color, color, color]
+        default:
+            return [color] // return 1 dot
+        }
+    }
+    
+    func dotMarker(shouldMoveOnHighlightingOnDayView dayView: CVCalendarDayView) -> Bool {
+        return true
+    }
+    
+    func dotMarker(sizeOnDayView dayView: DayView) -> CGFloat {
+        return 13
+    }
     
     
+    func weekdaySymbolType() -> WeekdaySymbolType {
+        return .Short
+    }
+    
+    func selectionViewPath() -> ((CGRect) -> (UIBezierPath)) {
+        return { UIBezierPath(rect: CGRectMake(0, 0, $0.width, $0.height)) }
+    }
+    
+    func shouldShowCustomSingleSelection() -> Bool {
+        return false
+    }
+    
+    func preliminaryView(viewOnDayView dayView: DayView) -> UIView {
+        let circleView = CVAuxiliaryView(dayView: dayView, rect: dayView.bounds, shape: CVShape.Circle)
+        circleView.fillColor = .colorFromCode(0xCCCCCC)
+        return circleView
+    }
+    
+    func preliminaryView(shouldDisplayOnDayView dayView: DayView) -> Bool {
+        if (dayView.isCurrentDay) {
+            return true
+        }
+        return false
+    }
+    
+    func supplementaryView(viewOnDayView dayView: DayView) -> UIView {
+        let π = M_PI
+        
+        let ringSpacing: CGFloat = 3.0
+        let ringInsetWidth: CGFloat = 1.0
+        let ringVerticalOffset: CGFloat = 1.0
+        var ringLayer: CAShapeLayer!
+        let ringLineWidth: CGFloat = 4.0
+        let ringLineColour: UIColor = UIColor.mahogany()//.blueColor()
+        
+        let newView = UIView(frame: dayView.bounds)
+        
+        let diameter: CGFloat = (newView.bounds.width) - ringSpacing
+        let radius: CGFloat = diameter / 2.0
+        
+        let rect = CGRectMake(newView.frame.midX-radius, newView.frame.midY-radius-ringVerticalOffset, diameter, diameter)
+        
+        ringLayer = CAShapeLayer()
+        newView.layer.addSublayer(ringLayer)
+        
+        ringLayer.fillColor = nil
+        ringLayer.lineWidth = ringLineWidth
+        ringLayer.strokeColor = ringLineColour.CGColor
+        
+        let ringLineWidthInset: CGFloat = CGFloat(ringLineWidth/2.0) + ringInsetWidth
+        let ringRect: CGRect = CGRectInset(rect, ringLineWidthInset, ringLineWidthInset)
+        let centrePoint: CGPoint = CGPointMake(ringRect.midX, ringRect.midY)
+        let startAngle: CGFloat = CGFloat(-π/2.0)
+        let endAngle: CGFloat = CGFloat(π * 2.0) + startAngle
+        let ringPath: UIBezierPath = UIBezierPath(arcCenter: centrePoint, radius: ringRect.width/2.0, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        ringLayer.path = ringPath.CGPath
+        ringLayer.frame = newView.layer.bounds
+        
+        return newView
+    }
+    
+    func supplementaryView(shouldDisplayOnDayView dayView: DayView) -> Bool {
+        //demo
+        //        if (Int(arc4random_uniform(3)) == 1) {
+        //            return true
+        //        }
+        
+        return false
+    }
+}
+
+
+// MARK: - CVCalendarViewAppearanceDelegate
+
+extension CalViewController: CVCalendarViewAppearanceDelegate {
+    func dayLabelPresentWeekdayInitallyBold() -> Bool {
+        return false
+    }
+    
+    func spaceBetweenDayViews() -> CGFloat {
+        return 2
+    }
+}
+
+// MARK: - IB Actions
+
+extension CalViewController {
+    @IBAction func switchChanged(sender: UISwitch) {
+        if sender.on {
+            calendarView.changeDaysOutShowingState(false)
+            shouldShowDaysOut = true
+        } else {
+            calendarView.changeDaysOutShowingState(true)
+            shouldShowDaysOut = false
+        }
+    }
+    
+    @IBAction func todayMonthView() {
+        calendarView.toggleCurrentDayView()
+    }
+    
+    /// Switch to WeekView mode.
+    @IBAction func toWeekView(sender: AnyObject) {
+        calendarView.changeMode(.WeekView)
+    }
+    
+    /// Switch to MonthView mode.
+    @IBAction func toMonthView(sender: AnyObject) {
+        calendarView.changeMode(.MonthView)
+    }
+    
+    @IBAction func loadPrevious(sender: AnyObject) {
+        calendarView.loadPreviousView()
+    }
     
     
+    @IBAction func loadNext(sender: AnyObject) {
+        calendarView.loadNextView()
+    }
+}
+
+// MARK: - Convenience API Demo
+
+extension CalViewController {
+    func toggleMonthViewWithMonthOffset(offset: Int) {
+        let calendar = NSCalendar.currentCalendar()
+        //        let calendarManager = calendarView.manager
+        let components = Manager.componentsForDate(NSDate()) // from today
+        
+        components.month += offset
+        
+        let resultDate = calendar.dateFromComponents(components)!
+        
+        self.calendarView.toggleViewWithDate(resultDate)
+    }
+    
+    func didShowNextMonthView(date: NSDate)
+    {
+        //        let calendar = NSCalendar.currentCalendar()
+        //        let calendarManager = calendarView.manager
+        let components = Manager.componentsForDate(date) // from today
+        
+        print("Showing Month: \(components.month) : \(date)")
+        
+        monthLabel.text = CVDate(date: date).globalDescription //スワイプで次月へ移動時に年月ラベルを次月に更新
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    func didShowPreviousMonthView(date: NSDate)
+    {
+        //        let calendar = NSCalendar.currentCalendar()
+        //        let calendarManager = calendarView.manager
+        let components = Manager.componentsForDate(date) // from today
+        
+        print("Showing Month: \(components.month) : \(date)")
+        monthLabel.text = CVDate(date: date).globalDescription //スワイプで前月へ移動時に年月ラベルを前月に更新
+    }
     
 }
+
---
+++ member'sCal/UIColor+Ext.swift
@@ -0,0 +1,47 @@
+//
+//  UIColor+Ext.swift
+//  member'sCal
+//
+//  Created by Junichi Sakonaka on 2016/03/22.
+//  Copyright © 2016年 yoshidajumokui.llc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    // http://www.colordic.org/w/
+    
+    class func sunday() -> UIColor {
+        return hexStr("d70035", alpha: 1.0) // carmine
+    }
+    
+    class func saturday() -> UIColor {
+        return hexStr("26499d", alpha: 1.0) // oriental blue
+    }
+
+    class func kurocha() -> UIColor {
+        return hexStr("583822", alpha: 1.0)
+    }
+    
+    class func mahogany() -> UIColor {
+        return hexStr("6b3f31", alpha: 1.0)
+    }
+    
+    
+    class func hexStr (var hexStr : NSString, let alpha : CGFloat) -> UIColor {
+        hexStr = hexStr.stringByReplacingOccurrencesOfString("#", withString: "")
+        let scanner = NSScanner(string: hexStr as String)
+        var color: UInt32 = 0
+        if scanner.scanHexInt(&color) {
+            let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
+            let g = CGFloat((color & 0x00FF00) >> 8) / 255.0
+            let b = CGFloat(color & 0x0000FF) / 255.0
+            return UIColor(red:r,green:g,blue:b,alpha:alpha)
+        } else {
+            print("invalid hex string")
+            return UIColor.whiteColor();
+        }
+    }
+}
